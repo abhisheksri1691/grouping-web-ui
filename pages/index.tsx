583973b991892fa6393652d43772f568dd07bdaa
abhishek srivastava <abhisheksri.dev@gmail.com>
2023-05-11 22:41:12 +0530

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Form from "./signUp";
+import Header from "./header";
+import MyFooter from "./MyFooter";
+import Registration from "./SignIn";
+import HeroHome from "./HeroHome";
+import Modal from "./Modal";
+
+export default function Home() {
+  return (
+    <>
+     <HeroHome/> 
+     {/* <Modal/>  */}
+    </>
+  )
+}
